36743aedd49d186e4bfc35cb7695b6df2d2aaad7
jonasbj99 <99955044+jonasbj99@users.noreply.github.com>
2022-05-03 10:46:16 +0200

    dsa

--- P2-Moodle-Unity/Assets/Scripts/PanelNavigation.cs
+++ P2-Moodle-Unity/Assets/Scripts/PanelNavigation.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PanelNavigation : MonoBehaviour
 {
+    //List of game objects accessible in the Unity inspector
     [SerializeField] GameObject[] panels;
 
+    //
     public void PanelChange(GameObject activePanel)
     {
         for (int i = 0; i < panels.Length; i++)
