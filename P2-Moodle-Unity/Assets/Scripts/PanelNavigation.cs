4519f2730585883bc3196641b8a8b84a51529313
jonasbj99 <99955044+jonasbj99@users.noreply.github.com>
2022-05-11 10:55:05 +0200

    Fixed

--- P2-Moodle-Unity/Assets/Scripts/PanelNavigation.cs
+++ P2-Moodle-Unity/Assets/Scripts/PanelNavigation.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 //https://www.youtube.com/watch?v=Rfv5M14ZoGY&t=813s&ab_channel=InfoGamer
 public class PanelNavigation : MonoBehaviour
 {
-    //List of game objects accessible in the Unity inspector
+    //Array of GameObjects
     [SerializeField] GameObject[] panels;
 
-    //
+    //A for loop deactivates all panels and the activePanel is activated
     public void PanelChange(GameObject activePanel)
     {
         for (int i = 0; i < panels.Length; i++)
