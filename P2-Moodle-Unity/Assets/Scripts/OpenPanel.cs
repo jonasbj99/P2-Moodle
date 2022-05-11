72ac4aac83fbe84b8b035b35558a6b7856db5bb3
LarsenLasse <lasselarsen00@gmail.com>
2022-05-11 11:23:11 +0200

    Dag lavet på ny branch

---
+++ P2-Moodle-Unity/Assets/Scripts/OpenPanel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPanel : MonoBehaviour
+{
+    //Defining the panel gameobject
+    public GameObject Panel;
+
+    public void PanelOpen()
+    {
+        //Checking if the panel is assigned to the button. If it is, the panel is set to active meaning it becomes visible.
+        if(Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+}
+
+//Lavet vha. youtube tutorial: https://www.youtube.com/watch?v=LziIlLB2Kt4
